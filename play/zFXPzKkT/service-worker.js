7bec34b8187457099ca3009ee612d25ec2fb9661
Shikua1422 <c1004120@planet.kanazawa-it.ac.jp>
2026-02-04 12:43:36 +0900

    Create service-worker.js

---
+++ play/zFXPzKkT/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "now-playing-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.PNG",
+  "/icon-512.PNG"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
